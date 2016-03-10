6cc0d5cd4a14b97459bf002bc1bb67a569cee690
rossgabay <rgabay@rgabay.com>
2016-03-10 14:05:21 -0500

    added HTTP POST support to add new data

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 var connectString = process.env.CONNECT_STRING || 'mongodb://localhost/dataAPI';
 
@@ -10,9 +11,9 @@ db.once('open', function() {
   console.log('connected to mongo @' + connectString);
 });
 
-var Data = require('./models/dataModel');
+/*var Data = require('./models/dataModel');
 
-/*var drec = new Data({fname: 'ftest', lname: 'ltest'});
+var drec = new Data({fname: 'ftest', lname: 'ltest'});
 drec.save(function(err, d){
 	if(err) console.error(err);
 	console.log('drec saved ok');
@@ -21,38 +22,9 @@ drec.save(function(err, d){
 var app = express();
 var port = process.env.PORT || 3000;
 
-var dataRouter = express.Router();
-
-dataRouter.route('/data').get(function(req, res){
-	
-	var query = {};
-	if(req.query.lname){
-		query.lname = req.query.lname;
-	}
-
-	Data.find(query,function(err, fdata){
-		
-			if(err){console.log(err);
-					res.status(500).send(err);
-			} else res.json(fdata);
-	})
-});
-
-dataRouter.route('/data/:id').get(function(req, res){
-	
-	if(req.query.lname){
-		query.lname = req.query.lname;
-	}
-
-	Data.findById(req.params.id,function(err, fdata){
-		
-			if(err){console.log(err);
-					res.status(500).send(err);
-			} 
-			else res.json(fdata);
-	})
-});
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+dataRouter = require("./routes/dataRoutes")();
 app.use("/api", dataRouter);
 
 app.get('/', function(req, res){
---
+++ routes/dataRoutes.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var Data = require('../models/dataModel');
+
+
+var routes = function(){
+	
+var dataRouter = express.Router();
+
+dataRouter.route('/data')
+	.post(function(req, res){
+		var dt = new Data(req.body);
+		dt.save();
+		res.status(201).send(dt);
+	})
+	.get(function(req, res){
+	var query = {};
+	if(req.query.lname){
+		query.lname = req.query.lname;
+	}
+
+	Data.find(query,function(err, fdata){
+		
+			if(err){console.log(err);
+					res.status(500).send(err);
+			} else res.json(fdata);
+	})
+});
+
+dataRouter.route('/data/:id').get(function(req, res){
+	
+	if(req.query.lname){
+		query.lname = req.query.lname;
+	}
+
+	Data.findById(req.params.id,function(err, fdata){
+		
+			if(err){console.log(err);
+					res.status(500).send(err);
+			} 
+			else res.json(fdata);
+	})
+});
+
+	return dataRouter;
+};
+
+module.exports = routes;
