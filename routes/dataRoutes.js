2d2681d8c5196fc4d0f24d9a604c9329ac3df947
rossgabay <rgabay@rgabay.com>
2016-03-14 12:07:12 -0400

    added callback on data save to POST and PUT handlers

--- routes/dataRoutes.js
+++ routes/dataRoutes.js
@@ -9,8 +9,15 @@ var dataRouter = express.Router();
 dataRouter.route('/data')
 	.post(function(req, res){
 		var dt = new Data(req.body);
-		dt.save();
-		res.status(201).send(dt);
+		dt.save(function(err){
+					if(err) {
+						console.log(err);
+						res.status(500).send(err);
+					} else {
+						res.status(201).send(dt);
+					}
+
+				});	
 	})
 	.get(function(req, res){
 	var query = {};
@@ -51,12 +58,20 @@ dataRouter.route('/data/:id').get(function(req, res){
 	res.json(req.fdata); //req.fdata populated by middleware setup
 })
  .put(function(req, res){
+ 				if(req.body._id) delete req.body._id;
+
 				req.fdata.fname = req.body.fname; //req.fdata populated by middleware setup
 				req.fdata.lname = req.body.lname;
-				fdata.save();
-				res.json(req.fdata);
+				req.fdata.save(function(err){
+					if(err) {
+						console.log(err);
+						res.status(500).send(err);
+					} else {
+						res.json(req.fdata);
+					}
 
- });
+				});
+ 	});
 
 	return dataRouter;
 };
